18b261d513565b819f06e4f64901c217ec90cdf0
iluxsm <be9.smile@gmail.com>
2020-12-14 12:46:31 +0900

    feat: 중위표기법 후위표기법 변환 함수

--- Calculator/Calculator/Model/DecimalCalculator.swift
+++ Calculator/Calculator/Model/DecimalCalculator.swift
@@ -6,3 +6,73 @@
 //
 
 import Foundation
+
+struct DecimalCalculator: Containing {
+    var formulaStack = Stack<String>()
+    var operatorStack = Stack<String>()
+    
+    private func prioritize(_ operator: String) -> Int {
+        switch `operator` {
+        case "+", "-":
+            return 1
+        case "*", "/":
+            return 2
+        default:
+            return 0
+        }
+    }
+    
+    private mutating func convertInfixToPostfix() -> [String] {
+        var postfixFormula: [String] = []
+        var temporaryStorage: [String] = []
+        
+        for element in formulaStack.storage {
+            if Double(element) != nil { // 숫자일 때
+                postfixFormula.append(element)
+            } else { // 연산자일 때
+                if !temporaryStorage.isEmpty && prioritize(temporaryStorage.last ?? "") >= prioritize(element) {
+                    postfixFormula.append(temporaryStorage.removeLast())
+                    if prioritize(element) >= prioritize(temporaryStorage.last ?? "") {
+                        postfixFormula.append(temporaryStorage.removeLast())
+                        temporaryStorage.append(element)
+                    }
+                } else {
+                    temporaryStorage.append(element)
+                }
+            }
+        }
+        while !temporaryStorage.isEmpty {
+            postfixFormula.append(temporaryStorage.removeLast())
+        }
+        
+        formulaStack.storage.removeAll()
+        
+        return postfixFormula
+    }
+        
+    func multiply() {
+        
+    }
+    
+    func divide() {
+        
+    }
+
+    
+    func add() {
+        <#code#>
+    }
+    
+    func subtract() {
+        <#code#>
+    }
+    
+    func sum() {
+        <#code#>
+    }
+    
+    func clear() {
+        <#code#>
+    }
+    
+}
