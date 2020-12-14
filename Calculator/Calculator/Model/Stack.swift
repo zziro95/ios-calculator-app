a88f7a551a53a84eb356a2128fc9e324d852df65
iluxsm <be9.smile@gmail.com>
2020-12-14 12:45:24 +0900

    feat: 제네릭타입 스택 구현

--- Calculator/Calculator/Model/Stack.swift
+++ Calculator/Calculator/Model/Stack.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct Stack<T> {
+    var storage: [T] = []
+    
+    mutating func push(_ value: T) {
+        storage.append(value)
+    }
+    mutating func pop() -> T? {
+        return storage.popLast()
+    }
+}
