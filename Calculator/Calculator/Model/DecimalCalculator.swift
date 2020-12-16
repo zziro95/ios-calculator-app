09863c6a59423d6de30ef1fc50cc5e1cfc046242
iluxsm <be9.smile@gmail.com>
2020-12-16 17:38:48 +0900

    feat: 10진 계산기 사칙연산 구현

--- Calculator/Calculator/Model/DecimalCalculator.swift
+++ Calculator/Calculator/Model/DecimalCalculator.swift
@@ -9,8 +9,43 @@ import Foundation
 
 struct DecimalCalculator: Containing {
     var formulaStack = Stack<String>()
-    var operatorStack = Stack<String>()
     
+    func add(_ formula: inout [Double]) -> Double {
+        let operand1 = formula.popLast() ?? 0
+        let operand2 = formula.popLast() ?? operand1
+        return operand1 + operand2
+    }
+    
+    func subtract(_ formula: inout [Double]) -> Double {
+        let operand1 = formula.popLast() ?? 0
+        let operand2 = formula.popLast() ?? operand1
+        return operand2 - operand1
+    }
+    
+    func multiply(_ formula: inout [Double]) -> Double {
+        let operand1 = formula.popLast() ?? 0
+        let operand2 = formula.popLast() ?? operand1
+        return operand1 * operand2
+    }
+    
+    func divide(_ formula: inout [Double]) -> Double {
+        let operand1 = formula.popLast() ?? 0
+        let operand2 = formula.popLast() ?? operand1
+        
+        return operand1 != 0 ? operand2 / operand1 : Double.infinity
+    }
+    
+    mutating func sum() -> Double {
+        return calculatePostfix()
+    }
+    
+    mutating func clear() {
+        formulaStack.storage.removeAll()
+    }
+}
+
+//MARK:- Extensions
+extension DecimalCalculator {
     private func prioritize(_ operator: String) -> Int {
         switch `operator` {
         case "+", "-":
@@ -27,52 +62,55 @@ struct DecimalCalculator: Containing {
         var temporaryStorage: [String] = []
         
         for element in formulaStack.storage {
-            if Double(element) != nil { // 숫자일 때
+            if Double(element) != nil {
                 postfixFormula.append(element)
-            } else { // 연산자일 때
+            } else {
                 if !temporaryStorage.isEmpty && prioritize(temporaryStorage.last ?? "") >= prioritize(element) {
                     postfixFormula.append(temporaryStorage.removeLast())
-                    if prioritize(element) >= prioritize(temporaryStorage.last ?? "") {
-                        postfixFormula.append(temporaryStorage.removeLast())
+                    if prioritize(element) > prioritize(temporaryStorage.last ?? "") {
+                        temporaryStorage.append(element)
+                    } else {
+                        postfixFormula.append(temporaryStorage.popLast() ?? "")
                         temporaryStorage.append(element)
                     }
+                    print(temporaryStorage)
+                    
                 } else {
                     temporaryStorage.append(element)
+                    print(temporaryStorage)
                 }
             }
         }
         while !temporaryStorage.isEmpty {
-            postfixFormula.append(temporaryStorage.removeLast())
+            postfixFormula.append(temporaryStorage.popLast() ?? "")
         }
-        
-        formulaStack.storage.removeAll()
-        
         return postfixFormula
     }
+    
+    
+    private mutating func calculatePostfix() -> Double {
+        let formula = convertInfixToPostfix()
+        var typeConvertedPostfixFormula: [Double] = []
         
-    func multiply() {
-        
+        for element in formula {
+            if let number = Double(element), Double(element) != nil {
+                typeConvertedPostfixFormula.append(number)
+            } else {
+                switch element {
+                case "+":
+                    typeConvertedPostfixFormula.append(add(&typeConvertedPostfixFormula))
+                case "-":
+                    typeConvertedPostfixFormula.append(subtract(&typeConvertedPostfixFormula))
+                case "*":
+                    typeConvertedPostfixFormula.append(multiply(&typeConvertedPostfixFormula))
+                case "/":
+                    typeConvertedPostfixFormula.append(divide(&typeConvertedPostfixFormula))
+                default:
+                    break
+                }
+            }
+        }
+        return typeConvertedPostfixFormula.last ?? 0
     }
-    
-    func divide() {
-        
-    }
-
-    
-    func add() {
-        <#code#>
-    }
-    
-    func subtract() {
-        <#code#>
-    }
-    
-    func sum() {
-        <#code#>
-    }
-    
-    func clear() {
-        <#code#>
-    }
-    
 }
+
